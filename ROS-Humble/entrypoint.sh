b5a2d1faa730fd2430b7ce5850c09c30b1893470
HanielUlises <hanielulises2003@gmail.com>
2026-04-02 20:16:50 -0600

    ROS Noetic and Gazebo classic for Fedora 43

--- ROS-Humble/entrypoint.sh
+++ ROS-Humble/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 [ -f /opt/ros/humble/setup.bash ] && source /opt/ros/humble/setup.bash
 [ -f /ros2_ws/install/setup.bash ] && source /ros2_ws/install/setup.bash
-
+a
 echo "========================================"
 echo "  ROS2 Humble + Ignition Gazebo"
 echo "  GPU: Intel iGPU / Mesa DRI"
